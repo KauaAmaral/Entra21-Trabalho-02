db8274e39baab2ce302c1d5c5a56cb37781f98bf
Guilherme DMueller <guilhermemuellerdzesigaleski@gmail.com>
2022-06-27 15:48:15 -0300

    CarrosForm iniciado

---
+++ TrabalhoWindowsForm/TrabalhoWindowsForm/CadastroCarrosForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabalhoWindowsForm
+{
+    public partial class CadastroCarrosForm : Form
+    {
+        private Button buttonApagar;
+        private DataGridView dataGridView1;
+        private Label labelNome;
+        private TextBox textBoxNome;
+        private Label labelPlaca;
+        private TextBox textBoxPlaca;
+        private Label labelMarca;
+        private Label labelCategoria;
+        private Button buttonEditar;
+
+        public CadastroCarrosForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.buttonEditar = new System.Windows.Forms.Button();
+            this.buttonApagar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelNome = new System.Windows.Forms.Label();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.labelPlaca = new System.Windows.Forms.Label();
+            this.textBoxPlaca = new System.Windows.Forms.TextBox();
+            this.labelMarca = new System.Windows.Forms.Label();
+            this.labelCategoria = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonEditar
+            // 
+            this.buttonEditar.Location = new System.Drawing.Point(357, 12);
+            this.buttonEditar.Name = "buttonEditar";
+            this.buttonEditar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEditar.TabIndex = 0;
+            this.buttonEditar.Text = "Editar";
+            this.buttonEditar.UseVisualStyleBackColor = true;
+            // 
+            // buttonApagar
+            // 
+            this.buttonApagar.Location = new System.Drawing.Point(494, 12);
+            this.buttonApagar.Name = "buttonApagar";
+            this.buttonApagar.Size = new System.Drawing.Size(75, 23);
+            this.buttonApagar.TabIndex = 1;
+            this.buttonApagar.Text = "Apagar";
+            this.buttonApagar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(596, 285);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // labelNome
+            // 
+            this.labelNome.AutoSize = true;
+            this.labelNome.Location = new System.Drawing.Point(603, 41);
+            this.labelNome.Name = "labelNome";
+            this.labelNome.Size = new System.Drawing.Size(40, 15);
+            this.labelNome.TabIndex = 3;
+            this.labelNome.Text = "Nome";
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Location = new System.Drawing.Point(603, 73);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(232, 23);
+            this.textBoxNome.TabIndex = 4;
+            // 
+            // labelPlaca
+            // 
+            this.labelPlaca.AutoSize = true;
+            this.labelPlaca.Location = new System.Drawing.Point(603, 109);
+            this.labelPlaca.Name = "labelPlaca";
+            this.labelPlaca.Size = new System.Drawing.Size(35, 15);
+            this.labelPlaca.TabIndex = 5;
+            this.labelPlaca.Text = "Placa";
+            // 
+            // textBoxPlaca
+            // 
+            this.textBoxPlaca.Location = new System.Drawing.Point(603, 127);
+            this.textBoxPlaca.Name = "textBoxPlaca";
+            this.textBoxPlaca.Size = new System.Drawing.Size(228, 23);
+            this.textBoxPlaca.TabIndex = 6;
+            // 
+            // labelMarca
+            // 
+            this.labelMarca.AutoSize = true;
+            this.labelMarca.Location = new System.Drawing.Point(603, 170);
+            this.labelMarca.Name = "labelMarca";
+            this.labelMarca.Size = new System.Drawing.Size(40, 15);
+            this.labelMarca.TabIndex = 7;
+            this.labelMarca.Text = "Marca";
+            // 
+            // labelCategoria
+            // 
+            this.labelCategoria.AutoSize = true;
+            this.labelCategoria.Location = new System.Drawing.Point(603, 251);
+            this.labelCategoria.Name = "labelCategoria";
+            this.labelCategoria.Size = new System.Drawing.Size(58, 15);
+            this.labelCategoria.TabIndex = 8;
+            this.labelCategoria.Text = "Categoria";
+            // 
+            // CadastroCarrosForm
+            // 
+            this.ClientSize = new System.Drawing.Size(847, 326);
+            this.Controls.Add(this.labelCategoria);
+            this.Controls.Add(this.labelMarca);
+            this.Controls.Add(this.textBoxPlaca);
+            this.Controls.Add(this.labelPlaca);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.labelNome);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonApagar);
+            this.Controls.Add(this.buttonEditar);
+            this.Name = "CadastroCarrosForm";
+            this.Text = "Cadastro  de Carros";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
