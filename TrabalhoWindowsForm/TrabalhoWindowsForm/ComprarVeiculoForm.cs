777663a931d562701bd562c1805e91a08f4f3ff9
Felipe Corrêa <105026938+FelipeECorrea@users.noreply.github.com>
2022-06-28 13:40:23 -0300

    Adicionado tela de Compra do veiculo

---
+++ TrabalhoWindowsForm/TrabalhoWindowsForm/ComprarVeiculoForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabalhoWindowsForm
+{
+    public partial class ComprarVeiculoForm : Form
+    {
+        public ComprarVeiculoForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
