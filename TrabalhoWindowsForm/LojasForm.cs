c65414c70de32b2499e17975caee65d1c79cb883
Kauã Amaral <kauaamaral10@gmail.com>
2022-06-29 22:03:02 -0300

    LojasForm Atualizada Adicionado metodos que precisam falta concluir

--- TrabalhoWindowsForm/LojasForm.cs
+++ TrabalhoWindowsForm/LojasForm.cs
@@ -2,9 +2,46 @@
 {
     public partial class LojasForm : Form
     {
+        private LojaServico lojaServico;
+        private ClienteServico clienteServico;
+
         public LojasForm()
         {
             InitializeComponent();
+
+            lojaServico = new LojaServico();
+
+            clienteServico = new ClienteServico();
+
+            PreencherDataGridViewComCliente();
         }
+
+        private void PreencherDataGridViewComCliente()
+        {
+            var clientes = clienteServico.ObterTodos();
+
+            for (int i = 0; i < clientes.Count; i++)
+            {
+                var cliente = clientes[i];
+                comboBoxCliente.Items.Add(cliente.Nome);
+            }
+        }
+
+        private void LimparCampos()
+        {
+            maskedTextBoxCnpj.Text = "";
+            textBoxRazaoSocial.Text = "";
+            textBoxNomeFantasia.Text = "";
+            radioButtonSim.Checked = true;
+            radioButtonNao.Checked = false;
+            checkBoxVendas.Checked = false;
+            checkBoxAlugueis.Checked = false;
+            checkBoxConsertos.Checked = false;
+
+            dataGridView1.ClearSelection();
+        }
+
+
+
     }
 }
